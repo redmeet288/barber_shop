827d2e480e3bef7141c438dfc49ff5e5d8086271
217 student <lunev.artem204@gmail.com>
2025-03-31 14:44:19 +0200

    allah akbar

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print("B")
-print("q")
+import asyncio
+from aiogram import Bot, Dispatcher
+
+
+async def main():
+    bot = Bot(token="7690304596:AAFdx-5KQNjHfkLN7c1d6jl9LoNduRk_gIA")
+    dp = Dispatcher()
+
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
